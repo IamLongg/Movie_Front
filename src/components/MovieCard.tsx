c68dc8c28ae98d615f2b01b0cf2e7c202db4665d
Long <86197008+IamLongg@users.noreply.github.com>
2026-04-22 21:14:20 +0700

    test code error

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface MovieCardProps {
-  movie: Movie;
+  movie: Moviesa;
 }
 
 export default function MovieCard({ movie }: MovieCardProps) {
